2b486849655ab5581cfb15b920e931d422fc4e38
= <=>
2022-11-08 00:19:47 +0400

    first commit for quick sort algo

---
+++ Quick_Sort_Algorithm/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Quick_Sort_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = { 25, 10, 5, 6, 30, 40, 2, 1, 18, 7 };
+            //int[] arr = { 25, 10, 1, 6, 30, 40, 5, 4, 18, 7 };
+            //int[] arr = {6, 10, 2, 3, 17, 9 };
+            //int[] arr = {6, 4, 10, 8, 1, 7, 2, 3, 17, 9 };
+            int[] arr = { 6, 4, 10, 8, 1, 7, 2, 3, 17, 9, 9, 65, 22, 54, 4545, 223, 56, 78 };
+            QuickSort(arr, arr.Length - 1, 0, arr.Length);
+
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        static void QuickSort(int[] arr, int pivotIndex, int j, int currentLenght)
+        {
+            if (currentLenght <= 1)
+                return;
+
+            int fixedBeginIndex = j;
+            int pivot = arr[pivotIndex];
+            int i = j - 1;
+
+            while (j <= pivotIndex)
+            {
+                if (pivot > arr[j])
+                {
+                    i++;
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+                if (j == pivotIndex)
+                {
+                    i++;
+                    arr[pivotIndex] = arr[i];
+                    arr[i] = pivot;
+                }
+                j++;
+            }
+
+            if (i != fixedBeginIndex && i > 0)
+            {
+                QuickSort(arr, i - 1, fixedBeginIndex, i);
+            }
+            if (i < pivotIndex)
+            {
+                QuickSort(arr, pivotIndex, i + 1, pivotIndex - i);
+            }
+        }
+    }
+}
